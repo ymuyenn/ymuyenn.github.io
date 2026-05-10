0981e7c903eb2a973fa35cbdd35645a291747bbc
ymuyenn <ymuyenn@gmail.com>
2026-05-10 09:02:36 +0000

    Deploying to gh-pages from @ ymuyenn/ymuyenn.github.io@235b06a7c1591215c41f2a95b34bd3b180929a4d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,18 +51,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
-      },{id: "post-a-post-with-image-galleries",
+            },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
         
