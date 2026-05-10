8124146b2c7b583e2a6af961ba95f84b626dbd9d
ymuyenn <ymuyenn@gmail.com>
2026-05-10 09:09:46 +0000

    Deploying to gh-pages from @ ymuyenn/ymuyenn.github.io@3c049752d6ca4a64582db2e122c96116fb2ba1a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -77,17 +77,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/advanced-images/";
           
         },
-      },{id: "post-a-post-with-vega-lite",
-        
-          title: "a post with vega lite",
-        
-        description: "this is what included vega lite code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/vega-lite/";
-          
-        },
       },{id: "post-a-post-with-echarts",
         
           title: "a post with echarts",
