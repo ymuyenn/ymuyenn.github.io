70214a9e24785668394e03312ca2e5f1a72a430b
ymuyenn <ymuyenn@gmail.com>
2026-05-10 09:05:04 +0000

    Deploying to gh-pages from @ ymuyenn/ymuyenn.github.io@91ac34d72859ce362a707ef4509beb41229a717c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -352,17 +352,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/math/";
           
         },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/code/";
-          
-        },
       },{id: "post-a-post-with-formatting-and-links",
         
           title: "a post with formatting and links",
