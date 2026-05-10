761f664b897363bb70311c97a6a19c0324ee2165
ymuyenn <ymuyenn@gmail.com>
2026-05-10 09:06:20 +0000

    Deploying to gh-pages from @ ymuyenn/ymuyenn.github.io@7327fa6ae8c25db0f9f7201be56fb37ef8dca84e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -132,17 +132,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/vega-lite/";
           
         },
-      },{id: "post-a-post-with-geojson",
-        
-          title: "a post with geojson",
-        
-        description: "this is what included geojson code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/geojson-map/";
-          
-        },
       },{id: "post-a-post-with-echarts",
         
           title: "a post with echarts",
