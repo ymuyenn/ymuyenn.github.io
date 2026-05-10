0404a9111b77808c673fc54821c087bb84707472
ymuyenn <ymuyenn@gmail.com>
2026-05-10 09:16:51 +0000

    Deploying to gh-pages from @ ymuyenn/ymuyenn.github.io@9bf52cd33d47e10cd0815200ae2d5961c12e3625 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -77,17 +77,6 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "post-a-post-with-redirect",
-        
-          title: "a post with redirect",
-        
-        description: "you can also redirect to assets like pdf",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/assets/pdf/example_pdf.pdf";
-          
-        },
       },{id: "post-a-post-with-diagrams",
         
           title: "a post with diagrams",
