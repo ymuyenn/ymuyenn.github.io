34f025935238ff053decb4790139e6025246baed
ymuyenn <ymuyenn@gmail.com>
2026-05-10 09:11:22 +0000

    Deploying to gh-pages from @ ymuyenn/ymuyenn.github.io@65158fc62198064b2ce32a8a4acd118fd82fe79d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,17 +55,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
       },{id: "post-a-post-with-tikzjax",
         
           title: "a post with TikZJax",
